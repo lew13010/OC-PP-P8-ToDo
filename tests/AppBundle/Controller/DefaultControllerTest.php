55f6f7b7979181cb0fd56ebe3e493587e6859128
LEW-PC\Lew <trancon.loic@gmail.com>
2017-07-09 12:09:05 +0200

    Tests unitaires et Fonctionnels

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -3,16 +3,47 @@
 namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testIndexWithoutLogin()
     {
         $client = static::createClient();
+        $client->request('GET', '/');
+        $this->assertEquals(Response::HTTP_UNAUTHORIZED, $client->getResponse()->getStatusCode());
+    }
 
-        $crawler = $client->request('GET', '/');
+    public function testIndexErrorUser()
+    {
+        $client = static::createClient(array(), array(
+            'PHP_AUTH_USER' => 'user',
+            'PHP_AUTH_PW'   => 'user',
+        ));
+        $client->request('GET', '/');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertEquals(Response::HTTP_UNAUTHORIZED, $client->getResponse()->getStatusCode());
+    }
+
+    public function testIndexUser()
+    {
+        $client = static::createClient(array(), array(
+            'PHP_AUTH_USER' => 'test',
+            'PHP_AUTH_PW'   => 'test',
+        ));
+        $client->request('GET', '/');
+
+        $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+    }
+
+    public function testIndexAdmin()
+    {
+        $client = static::createClient(array(), array(
+            'PHP_AUTH_USER' => 'admin',
+            'PHP_AUTH_PW'   => 'admin',
+        ));
+        $client->request('GET', '/');
+
+        $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
     }
 }
